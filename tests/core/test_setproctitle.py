da420f4f4c3bfcd9af6492924a5e6aba56a1022d
jincao <jincao@126.com>
2021-09-10 20:26:39 +0800

    1

--- tests/core/test_setproctitle.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from peas.util.setproctitle import setproctitle
-
-
-class TestSetProcTitle(unittest.TestCase):
-    def test_does_not_crash(self):
-        setproctitle("peas test title")
